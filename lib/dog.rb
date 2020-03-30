8a701769c274f43a2b91bd4c947996fe8feb055e
CiaraMaria Proia <proiacm@gmail.com>
2020-03-30 20:26:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,24 +4,12 @@ class Dog
     @name = name
   end
   
-  def name=(name)
-    @name = name
-  end
-  
-  def name
-    @name 
-  end
+ 
 
 
   def initialize(breed)
    @breed = breed
   end
   
-  def breed=(breed)
-    @breed = breed
-  end
   
-  def breed
-    @breed
-  end
 end
